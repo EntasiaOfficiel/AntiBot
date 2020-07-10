88def7c218687886d0697fb30f644f61f31b235a
iTrooz_ <team.mortelgames@gmail.com>
2020-07-10 20:12:21 +0200

    creation (et c'est parti pour les noms de commit)

---
+++ src/fr/entasia/antibot/utils/AntibotLevel.java
@@ -0,0 +1,28 @@
+package fr.entasia.antibot.utils;
+
+import fr.entasia.antibot.Utils;
+
+public enum AntibotLevel {
+	PING, CAPTCHA;
+
+	public int id;
+
+	AntibotLevel(){
+		AntibotLevel[] a = values();
+		for(int i=0;i<a.length;i++){
+			if(a[i]==this){
+				id = i;
+				return;
+			}
+		}
+	}
+
+	public static boolean isActive(){
+		return Utils.level!=null;
+	}
+
+	public static boolean activeMode(AntibotLevel compare){
+		if(compare.id <= Utils.level.id)return true;
+		else return true;
+	}
+}
---
+++ src/fr/entasia/antibot/utils/AntibotMode.java
@@ -0,0 +1,5 @@
+package fr.entasia.antibot.utils;
+
+public enum AntibotMode { // a voir pour ajouter SLEEP ?
+	OFF, ON, STRICT
+}
